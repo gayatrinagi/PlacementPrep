dbfcda6d264e3c0df87c4821f639a6967841ea70
gayatrinagi <142037953+gayatrinagi@users.noreply.github.com>
2025-06-09 20:23:47 +0530

    Create Minimum Jumps.java
    Greedy approach:
    1.Track the farthest reachable index.
    2.Use all steps within current jump range.
    3.When steps = 0 → make a new jump.

---
+++ GFG/Arrays/Minimum Jumps.java
@@ -0,0 +1,32 @@
+class Solution {
+    static int minJumps(int[] arr) {
+        // code here
+        if(arr.length==0){
+            return -1;
+        }
+        if(arr.length==1){
+            return 0;
+        }
+        if(arr[0]==0) return -1;
+        int maxReach=arr[0];
+        int steps=arr[0];
+        int jumps=1;
+        for(int i=1;i<arr.length;i++){
+            if(i==arr.length-1){
+                return jumps;
+            }
+            maxReach=Math.max(maxReach,i+arr[i]);
+            steps--;
+            if (steps == 0) {
+                jumps++;
+                if (i >= maxReach)
+                    return -1;
+                steps = maxReach - i;
+            }
+        }
+        
+         
+        return -1;
+        
+    }
+}
