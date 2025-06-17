98ce6ed220a60c50aa0c5b6b023cdae07553ba83
gayatrinagi <142037953+gayatrinagi@users.noreply.github.com>
2025-06-17 22:39:03 +0530

    Update Closest in BST.java

--- GFG/BinaryTrees/Closest in BST.java
+++ GFG/BinaryTrees/Closest in BST.java
@@ -38,3 +38,46 @@ class Solution {
         
     }
 }
+/*approach2 starts here better approach
+class Node
+{
+    int data;
+    Node left, right;
+
+    public Node(int d)
+    {
+        data = d;
+        left = right = null;
+    }
+}
+
+
+class Solution {
+    // Function to find the least absolute difference between any node
+    // value of the BST and the given integer.
+    static int min(Node root,int k){
+        if(root==null) return Integer.MAX_VALUE;
+        int closest=root.data;
+        while(root!=null){
+        if(Math.abs(root.data-k)<Math.abs(closest-k)){
+            closest=root.data;
+        }
+        if(k<root.data){
+         root=root.left;
+        }else if(k>root.data){
+          root=root.right;
+        }else{
+            break;
+        }
+    }
+    return Math.abs(closest - k);
+    }
+    static int minDiff(Node root, int K) {
+        // Write your code here
+        if(root==null){
+            return Integer.MAX_VALUE;
+        }
+        return min(root,K);
+    }
+}
+*/
